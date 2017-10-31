6f6751adc8f650bcd94d0736a71592df467a4eb1
DESKTOP-CB4SSJ3\Colin <coliwob@gmail.com>
2017-10-31 16:17:24 +0000

    FileFinder, try catch.

---
+++ FileFinder/FileFinder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace FileFinder
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string filename = null;
+			try
+			{
+				filename = args[0];
+
+				using (StreamReader reader = File.OpenText(filename))
+				{
+					string contents = reader.ReadToEnd();
+				}
+			}
+			catch (IndexOutOfRangeException ex)
+			{
+				Console.Error.WriteLine("No filename specified");
+				Environment.Exit(1);
+			}
+			catch (FileNotFoundException ex)
+			{
+				Console.Error.WriteLine("File \"{0}\" doen not exist.", filename);
+				Environment.Exit(2);
+			}
+			catch (Exception ex)
+			{
+				Console.Error.WriteLine(ex.Message);
+				Environment.Exit(3);
+			}
+			finally
+			{
+				Console.WriteLine("Done");
+			}
+		}
+	}
+}
