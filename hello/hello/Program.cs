b85756a7641117803a2d214a9a268e239c856992
DESKTOP-CB4SSJ3\Colin <coliwob@gmail.com>
2017-10-31 14:19:37 +0000

    helloworld, again. FFS

---
+++ hello/hello/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace hello
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				Console.Error.WriteLine("Enter your name!");
+				Environment.Exit(-1);
+			}
+
+			string name = args[0];
+			Console.WriteLine("Hello, {0}!", name);
+
+			if (args.Length > 1)
+			{
+				foreach (string arg in args)
+				{
+					Console.WriteLine(arg);
+				}
+			}
+		}
+	}
+}
