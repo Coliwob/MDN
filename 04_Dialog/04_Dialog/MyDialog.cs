227fcc4ab02be4f56781789fa8fbadefe395d3ab
DESKTOP-CB4SSJ3\Colin <coliwob@gmail.com>
2017-11-02 19:45:44 +0000

    04-Dialog

---
+++ 04_Dialog/04_Dialog/MyDialog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Gtk;
+namespace _Dialog
+{
+	public partial class MyDialog : Gtk.Dialog
+	{
+		public MyDialog(string title, string question, string explanation)
+		{
+			this.Title = title;
+			this.HasSeparator = false;
+			this.BorderWidth = 6;
+			this.Resizable = false;
+
+			HBox h = new HBox();
+			h.BorderWidth = 6;
+			h.Spacing = 12;
+
+			Image i = new Image();
+			i.SetFromStock(Stock.DialogQuestion, IconSize.Dialog);
+			i.SetAlignment(0.5f, 0);
+			h.PackStart(i, false, false, 0);
+
+			VBox v = new VBox();
+			Label l = new Label("<span weight=\"bold\" size=\"larger\">" + question + "</span>");
+			l.LineWrap = true;
+			l.UseMarkup = true;
+			l.Selectable = true;
+			l.Xalign = 0;
+			l.Yalign = 0;
+			v.PackStart(l);
+
+			l = new Label(explanation);
+			l.LineWrap = true;
+			l.Selectable = true;
+			l.Xalign = 0;
+			l.Yalign = 0;
+			v.PackEnd(l);
+
+			h.PackEnd(v);
+			h.ShowAll();
+			this.VBox.Add(h);
+
+			this.AddButton(Stock.Cancel, ResponseType.Cancel);
+			this.AddButton(Stock.Ok, ResponseType.Ok);
+
+		}
+	}
+}
---
+++ 04_Dialog/04_Dialog/program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+using _Dialog;
+
+public class MainClass
+{
+	public static void Main(string[] args)
+	{
+		Application.Init();
+
+		MyDialog d = new MyDialog("Catering Request", "Would you like fries with that?", "Beware though, they taste boggin.");
+
+		ResponseType resp = (ResponseType)d.Run();
+
+		if (resp == ResponseType.Ok)
+		{
+			Console.WriteLine("You got fat");
+		}
+		else if (resp == ResponseType.Cancel)
+		{
+			Console.WriteLine("You didnt got fat");
+		}
+
+
+	}
+}
