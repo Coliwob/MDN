b2122502547209da81187d715d17bdba7958ac32
DESKTOP-CB4SSJ3\Colin <coliwob@gmail.com>
2017-11-07 13:56:04 +0000

    04 - Subclass

---
+++ 04_SubClassing/04_SubClassing/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Gtk;
+
+
+class MainClass
+{
+
+	public static void Main(string[] args)
+	{
+		Application.Init();
+
+		Window w = new Window("Hit Count");
+		MyButton b = new MyButton("Hit Count");
+		b.Clicked += Button_Clicked;
+
+		w.DeleteEvent += Delete_Event;
+		w.Add(b);
+		w.ShowAll();
+		Application.Run();
+	}
+
+	public static void Button_Clicked(object sender, EventArgs o)
+	{
+		MyButton b = (MyButton)sender;
+		Console.WriteLine("Clicked {0} times", b.HitCount);
+	}
+
+	public static void Delete_Event(object sender, DeleteEventArgs e)
+	{
+		Application.Quit();
+		e.RetVal = true;
+	}
+}
+
+class MyButton : Button
+{
+
+	private int hitcount;
+
+	public MyButton(string text) : base(text)
+	{
+		hitcount = 0;
+	}
+
+	protected override void OnClicked()
+	{
+		hitcount++;
+		base.OnClicked();
+	}
+
+	public int HitCount
+	{
+		get { return hitcount; }
+		set { hitcount = value; }
+	}
+
+}
