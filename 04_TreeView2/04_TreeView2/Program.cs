e237c7d3473110015aa17b1d21020fb90a86e60a
DESKTOP-CB4SSJ3\Colin <coliwob@gmail.com>
2017-11-09 15:49:11 +0000

    Treeview

--- 04_TreeView2/04_TreeView2/Program.cs
+++ 04_TreeView2/04_TreeView2/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using Gtk;
-
+using Gdk;
 public class Program
 {
+	private static Pixbuf png;
 	public static void Main(string[] args)
 	{
 		Application.Init();
-		Window w = new Window("TreeView - Tree Shaped Data");
+
+		png = new Pixbuf("b:/mdn/16x16.png");
+
+		Gtk.Window w = new Gtk.Window("TreeView - Tree Shaped Data");
+
 		w.DeleteEvent += Window_Delete;
 		VBox v = new VBox();
 		v.BorderWidth = 16;
@@ -17,25 +22,28 @@ public class Program
 		v.Add(tv);
 
 		TreeViewColumn col = new TreeViewColumn();
+		CellRendererPixbuf pixr = new CellRendererPixbuf();
+		col.PackStart(pixr, false);
+		col.AddAttribute(pixr, "pixbuf", 1);
+
 		CellRenderer colr = new CellRendererText();
 		col.Title = "Column 1";
 		col.PackStart(colr, true);
 		col.AddAttribute(colr, "text", 0);
 		tv.AppendColumn(col);
 
-		TreeStore store = new TreeStore(typeof(string));
+
+
+		TreeStore store = new TreeStore(typeof(string), typeof(Pixbuf));
 		tv.Model = store;
 
 		TreeIter iter = new TreeIter();
 		for (int i = 0; i < 4; i++)
 		{
-			iter = store.AppendValues("Point " + i.ToString());
+			iter = store.AppendValues("Point " + i.ToString(), png);
 			for (int j = i - 1; j >= 0; j--)
 			{
-				iter = store.AppendValues(iter, "Visited " + j.ToString());
-				iter = store.AppendValues(iter, "Visited " + j.ToString());
-				iter = store.AppendValues(iter, "Visited " + j.ToString());
-				iter = store.AppendValues(iter, "Visited " + j.ToString());
+				store.AppendValues(iter, "Visited " + j.ToString(), png);
 			}
 		}
 
